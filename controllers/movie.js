6aa0bad99ac0350883969a0f5baeaed3065aa532
AskariTech <hasanaskari936@gmail.com>
2023-09-08 17:21:10 +0500

    added VIPLevel

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,7 +1,7 @@
 const Movie = require('../models/Movie'); 
 const StatusCodes = require('http-status-codes')
 const addMovie = async (req, res) => {
-  const { title,product,price } = req.body;
+  const { title,product,price,VIPLevel } = req.body;
 
   try {
     // Create a new movie instance
@@ -17,7 +17,7 @@ const addMovie = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ message: 'Movie added successfully', newMovie });
   } catch (error) {
     console.error('Error:', error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error',error });
   }
 };
 
